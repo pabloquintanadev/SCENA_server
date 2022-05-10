949af67da9d8c933da65b5a08ae57e6eb49a3b41
Clara Pardo <clara.pardo@hotmail.com>
2022-05-10 18:50:43 +0200

    routes pero mal

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -1,22 +1,53 @@
-const router = require("express").Router();
+const router = require("express").Router()
+
+const Event = require('./../models/Event.model')
+const User = require('./../models/User.model')
 
 router.get('/', (req, res) => {
-    res.json('Todos eventos')
+
+    Event
+        .find()
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
+
+router.post('/create', (req, res) => {
+    const { username, email, password, phoneNumber, title, date, mainArtist, secondaryArtists, venue, creator, aprovedArtist, aprovedVenue } = req.body
+
+    User
+        .create({ username, email, password, phoneNumber })
+        .then(createdUser => { Event.create({ user: createdUser._id, title, date, mainArtist, secondaryArtists, venue, isAproved: { mainArtist: aprovedArtist, venue: aprovedVenue}, creator })})
+        .then(createdEvent => res.json(createdEvent))
+        .catch(err => res.status(500).json(err))
+
 })
 
 router.get('/:id', (req, res) => {
     const { id } = req.params
-    res.json(id)
+
+    Event
+        .findById(id)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
 })
 
 router.post('/:id/delete', (req, res) => {
     const { id } = req.params
-    res.json(id)
+
+    Event
+        .findByIdAndDelete(id)
+        .then(() => res.status(200).json({ message: 'deleted correctly' }))
+        .catch(err => res.status(500).json(err))
 })
 
 router.post('/:id/edit', (req, res) => {
     const { id } = req.params
-    res.json(id)
+    const { title, date, mainArtist, secondaryArtists, venue, isApproved, creator } = req.body
+
+    Event
+        .findByIdAndUpdate(id, { title, date, mainArtist, secondaryArtists, venue, isApproved, creator })
+        .then(() => res.json(response))
+        .catch(err => res.status(500).json(err))
 })
 
 module.exports = router
--- routes/venues.routes.js
+++ routes/venues.routes.js
@@ -1,22 +1,43 @@
-const router = require("express").Router();
+const router = require("express").Router()
+
+const Venue = require('./../models/Venue.model')
+const User = require('./../models/User.model')
 
 router.get('/', (req, res) => {
-    res.json('Todos los venues')
+
+    Venue
+        .find()
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
 })
 
 router.get('/:venueId', (req, res) => {
     const { venueId } = req.params
-    res.json(venueId)
+
+    Venue
+        .findById(venueId)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
 })
 
 router.get('/search/:venueName', (req, res) => {
     const { venueName } = req.params
-    res.json(venueName)
+
+    // User
+    //     .find({ username: venueName })
+    
+    
+    // Venue
+    //     .find({ name: venueName })
+    //     .then(response => res.json(response))
+    //     .catch(err => res.status(500).json(err))
 })
 
 router.get('/search/location/:district', (req, res) => {
     const { district } = req.params
-    res.json(district)
+
+
+
 })
 
-module.exports = router; 
+module.exports = router
