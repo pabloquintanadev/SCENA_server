020d997e86465b15ad2d171e8208061ef0d08385
wablopilson <wablopilson@gmail.com>
2022-05-12 10:33:40 +0200

    cloudinary

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,11 +4,25 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+//upload routes
+router.use("/upload", require('./upload.routes'))
+
+//artists routes
 router.use('/artists', require('./artists.routes'))
+
+//fans routes
 router.use('/fans', require('./fan.routes'))
+
+//events routes
 router.use('/events', require('./events.routes'))
+
+//venues routes
 router.use('/venues', require('./venues.routes'))
+
+//labels routes
 router.use('/labels', require('./labels.routes'))
+
+//auth routes
 router.use('/auth', require('./auth.routes'))
 
 
